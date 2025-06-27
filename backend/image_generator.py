d416b3b2cf963e9f146d908b20ef4790eed4315e
Michael DeCero <madecero@protonmail.com>
2025-06-26 21:10:16 -0500

    Updated image model to SDXL

--- backend/image_generator.py
+++ backend/image_generator.py
@@ -8,7 +8,7 @@ def generate_images(story_chunks):
 
     for prompt in story_chunks:
         output = replicate_client.run(
-            "stability-ai/sdxl:8ab9d5c018148290b95ca7d94c640121be51dfb4641779aa0951f20a22c1111c",  # SDXL Turbo version
+            "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",  # SDXL
             input={"prompt": prompt}
         )
         images.append(output[0])  # First URL in list
